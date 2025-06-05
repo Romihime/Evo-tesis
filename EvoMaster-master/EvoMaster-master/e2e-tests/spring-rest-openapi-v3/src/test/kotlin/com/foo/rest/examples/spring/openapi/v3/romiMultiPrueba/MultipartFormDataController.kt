724d0b79b6acca0217865e8951476ad7bb0e008f
Romihime <romi_de_munro@hotmail.com>
2025-06-04 23:31:19 -0300

    falta probar el micro de multi

---
+++ EvoMaster-master/EvoMaster-master/e2e-tests/spring-rest-openapi-v3/src/test/kotlin/com/foo/rest/examples/spring/openapi/v3/romiMultiPrueba/MultipartFormDataController.kt
@@ -0,0 +1,7 @@
+package com.foo.rest.examples.spring.openapi.v3.romiMultiPrueba
+
+import com.foo.rest.examples.spring.openapi.v3.SpringController
+import com.foo.rest.examples.spring.openapi.v3.romiMultipartPrueba.MultipartFormDataApplication
+
+
+class MultipartFormDataController : SpringController(MultipartFormDataApplication::class.java)
